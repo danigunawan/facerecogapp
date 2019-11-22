00326c50dbc705e86fa25ae9e984920a153abb9c
Roberto Yanacon <robertoyanacon@gmail.com>
2019-11-22 10:59:13 -0300

    incorporacion de modelos

--- script.js
+++ script.js
@@ -26,3 +26,18 @@ function startVideo() {
 
 }
 
+video.addEventListener('play', () => {
+    const canvas = faceapi.createCanvasFromMedia(video);
+    const videoCont = document.getElementById('videoContainer');
+    videoCont.append(canvas)
+    const displaySize = { width: video.width, height: video.height }
+    faceapi.matchDimensions(canvas, displaySize)
+    setInterval(async () => {
+        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+        const resizeDetections = faceapi.resizeResults(detections, displaySize)
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height) //hace que el renderizado sea mas limpio
+        faceapi.draw.drawDetections(canvas, resizeDetections) //dibujo loque quiero visualizar
+        faceapi.draw.drawFaceLandmarks(canvas, resizeDetections)
+        faceapi.draw.drawFaceExpressions(canvas, resizeDetections)
+    }, 100)
+})
