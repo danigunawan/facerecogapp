89c4b7ed15b62ec144deba53d8ad3388f84dfc43
Roberto Yanacon <robertoyanacon@gmail.com>
2019-11-22 10:07:22 -0300

    incorporacion de api

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 const video = document.getElementById('video');
-const msj = document.getElementById('msj');
-msj.innerHTML = 'holis'
 
-if (navigator.getUserMedia) {
-	navigator.getUserMedia({video:{}}, successCallback, errorCallback);
-	function successCallback(stream) {
-		video.srcObject = stream;
-	}
-	function errorCallback(error) {
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/models')
+]).then(startVideo)
 
-        msj.innerHTML =   'An error occurred: [CODE ' + error.code + ']';
-		
-	}
-} else {
-	msj.innerHTML = 'Native web camera streaming (getUserMedia) is not supported in this browser.';
+
+function startVideo() {
+
+    if (navigator.getUserMedia) {
+        navigator.getUserMedia({ video: {} }, successCallback, errorCallback);
+        function successCallback(stream) {
+            video.srcObject = stream;
+        }
+        function errorCallback(error) {
+
+            console.log('An error occurred: [CODE ' + error.code + ']');
+
+        }
+    } else {
+        console.log('Native web camera streaming (getUserMedia) is not supported in this browser.');
+    }
+
 }
+
